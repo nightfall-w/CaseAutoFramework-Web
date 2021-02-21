a9a5e911a614b002b1668e34488252f325b8887c
nightfall-w <rimuli_w@163.com>
2021-02-21 23:46:01 +0800

    case job websocket

--- src/views/testplan/CaseTaskList.vue
+++ src/views/testplan/CaseTaskList.vue
@@ -4,42 +4,34 @@
  * @Author: wangbaojun
  * @Date: 2021-02-19 22:43:32
  * @LastEditors: wangbaojun
- * @LastEditTime: 2021-02-20 00:21:34
+ * @LastEditTime: 2021-02-21 18:02:40
 -->
 <template>
   <div>
     <el-table
       :data="tableData"
-      :row-key="record => record.id"
+      :row-key="(record) => record.id"
       :expand-row-keys="expands"
       @expand-change="expand_change"
       style="width: 100%"
     >
       <el-table-column type="expand">
         <template slot-scope="props">
-          <!-- <el-form label-position="left" inline class="table-expand">
-          <el-form-item label="商品名称">
-            <span>{{ props.row.name }}</span>
-          </el-form-item>
-          <el-form-item label="所属店铺">
-            <span>{{ props.row.shop }}</span>
-          </el-form-item>
-          <el-form-item label="商品 ID">
-            <span>{{ props.row.id }}</span>
-          </el-form-item>
-          <el-form-item label="店铺 ID">
-            <span>{{ props.row.shopId }}</span>
-          </el-form-item>
-          <el-form-item label="商品分类">
-            <span>{{ props.row.category }}</span>
-          </el-form-item>
-          <el-form-item label="店铺地址">
-            <span>{{ props.row.address }}</span>
-          </el-form-item>
-          <el-form-item label="商品描述">
-            <span>{{ props.row.desc }}</span>
-          </el-form-item>
-        </el-form> -->
+          <el-collapse v-model="activeNames" @change="handleChange">
+            <el-collapse-item
+              v-for="(item, i) in job_list"
+              :key="i"
+              title="一致性 Consistency"
+              name="1"
+            >
+              <div>
+                与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；
+              </div>
+              <div>
+                在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
+              </div>
+            </el-collapse-item>
+          </el-collapse>
         </template>
       </el-table-column>
       <el-table-column label="任务 ID" prop="id"> </el-table-column>
@@ -112,7 +104,9 @@ export default {
       defaultPageSize: 10,
       currentPage: 1,
       totalItems: 0,
-      expands: []
+      expands: [],
+      job_list: [],
+      activeNames: [],
     };
   },
   created() {
@@ -123,12 +117,12 @@ export default {
       this.defaultPageSize,
       0
     )
-      .then(res => {
+      .then((res) => {
         console.log(res);
         this.tableData = res.results;
         this.totalItems = res.count;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   },
@@ -137,10 +131,10 @@ export default {
   },
   computed: {
     percentage() {
-      return function(finish_num, case_job_number) {
+      return function (finish_num, case_job_number) {
         return (finish_num / case_job_number) * 100;
       };
-    }
+    },
   },
   methods: {
     initWebSocket() {
@@ -175,6 +169,8 @@ export default {
             }
           }
         }
+      } else if (redata.success && redata.mode === "job") {
+        console.log(redata);
       }
     },
     websocketsend(Data) {
@@ -204,22 +200,11 @@ export default {
         this.$route.query.case_testplan_uid,
         this.defaultPageSize,
         0
-      ).then(res => {
+      ).then((res) => {
         this.tableData = res.results;
         this.totalItems = res.count;
         console.log(this.websock.readyState);
         this.websock.close();
-        // while (true) {
-        //   if (this.websock == WebSocket.OPEN) {
-        //     this.websocketsend(this.package_ws_task_data());
-        //     break;
-        //   } else {
-        //     setTimeout(function() {
-        //       //可以是一句或是很多句代码，也可以是个函数
-        //     }, 1000); //延时10秒
-        //     continue;
-        //   }
-        // }
       });
     },
     handleCurrentChange(val) {
@@ -228,7 +213,7 @@ export default {
         this.$route.query.case_testplan_uid,
         this.defaultPageSize,
         (val - 1) * this.defaultPageSize
-      ).then(res => {
+      ).then((res) => {
         this.tableData = res.results;
         this.totalItems = res.count;
         this.websock.close();
@@ -241,12 +226,17 @@ export default {
         that.expands = [];
         if (row) {
           that.expands.push(row.id); // 只展开当前行id
+          console.log(this.package_ws_job_data());
+          this.websocketsend(this.package_ws_job_data());
         }
         //  this.tablaData(row.eqId)  这里可以调用接口数据渲染
       } else {
         // 说明收起了
         that.expands = [];
       }
+    },
+    handleChange(value) {
+      console.log(value);
     },
     package_ws_task_data() {
       return JSON.stringify({
@@ -255,10 +245,19 @@ export default {
         value: {
           case_test_plan_uid: this.$route.query.case_testplan_uid,
           limit: this.currentPage * this.defaultPageSize,
-          offset: (this.currentPage - 1) * this.defaultPageSize
-        }
+          offset: (this.currentPage - 1) * this.defaultPageSize,
+        },
       });
-    }
-  }
+    },
+    package_ws_job_data() {
+      return JSON.stringify({
+        mode_type: "case",
+        task_or_job: "job",
+        value: {
+          case_task_id: this.expands[0],
+        },
+      });
+    },
+  },
 };
 </script>
