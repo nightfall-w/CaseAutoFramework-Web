f13f9073aba7f7bb43e75fd2076b75660f7d7b78
nightfall-w <rimuli_w@163.com>
2021-04-01 21:44:56 +0800

    1. build打包压缩 2. 修改生产环境配置

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,18 @@
  * @version: 
  * @Author: wangbaojun
  * @Date: 2020-01-09 16:55:41
- * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2020-01-15 11:58:05
+ * @LastEditors: wangbaojun
+ * @LastEditTime: 2021-04-01 21:43:44
  */
 console.log("后端服务地址:" + process.env.VUE_APP_SERVER_URL)
-
+const webpack = require('webpack');
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ['js', 'css']
 module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 8081
+  },
   lintOnSave: true,
   configureWebpack: {
     resolve: {
@@ -20,6 +26,19 @@ module.exports = {
         'network': '@/network',
         'views': '@/views',
       }
-    }
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5,
+        minChunkSize: 100
+      })
+    ]
   }
-}
+                                                                                                                           1, 2           Top
